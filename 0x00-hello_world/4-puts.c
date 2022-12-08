3a0e972fffa705e2353fb51825349f5dfb9282e0
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-08 03:48:47 -0800

    puts and description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /*output a text */
 int main(void)
-
-
-
+/*
+ *main function operate all functions in .
+ */
 {
 
 char *str = "\"Programming is like building a multilingual puzzle";
