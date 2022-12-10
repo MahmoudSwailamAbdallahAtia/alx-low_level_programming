58f55a868796838a6eccffa0a2bfe356e381d8d2
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-10 06:49:40 -0800

    dsf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-/*description here*/
+/*description for function main  here*/
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+return (0); }
