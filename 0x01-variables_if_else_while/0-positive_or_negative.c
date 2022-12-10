e8816a60e4e52883b00c504add5f53a6b8710a7e
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-10 10:57:56 -0800

    try

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,10 @@
  *Return: 0
  */
 int main(void)
-{int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+{
+  int n;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
 if(n > 0)
 {printf("%dis positive\n", n);
 }
