3d50980fe1d20068cc9837e0c8dde608041534bb
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-10 10:17:56 -0800

    jhdfj

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main(){sizeof(int);
-printf("Size of a int: 4 bytes");
+int main(){sizeof(char);
+  printf("Size of a char: %d byte(s)", sizeof(char));
 sizeof (float);
 printf("Size of a float: 4 bytes");
 sizeof (double);
@@ -11,3 +11,13 @@ sizeof(unsigned int);
 printf("Size of a unsigned int: 8 bytes");
 return (0);
 }
+Size of a char: 1 byte(s)
+  
+  Size of an int: 4 byte(s)
+  
+  Size of a long int: 4 byte(s)
+  
+  Size of a long long int: 8 byte(s)
+  
+  Size of a float: 4 byte(s)
+  
