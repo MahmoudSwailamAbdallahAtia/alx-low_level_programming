d7c2799fa8319642bb1f04c8ce69935a9d75f6ad
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-10 09:57:32 -0800

    try

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
-int main (void){
-  printf "with proper grammar but the outcome is a piece of art"
-  return (0)
+#include <stdio.h>
+/**
+ *main - Entry man
+ *Return: 0
+ */
+int main(void)
+{printf("with proper grammar but the outcome is a piece of art,");
+return (0);
 }
