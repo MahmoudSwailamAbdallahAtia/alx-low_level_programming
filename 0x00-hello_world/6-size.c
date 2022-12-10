c0684604e28f262f9343f7aa6acfb7e71bf43df6
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-10 10:13:31 -0800

    size of everything in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main(){sizeof(int);
-
+printf("Size of a int: 4 bytes");
 sizeof (float);
-
+printf("Size of a float: 4 bytes");
 sizeof (double);
-
+printf("Size of a double: 8 bytes");
 sizeof(char);
-
-return(0);
-
+printf("Size of a char: 1 byte");
+sizeof(unsigned int);
+printf("Size of a unsigned int: 8 bytes");
+return (0);
 }
