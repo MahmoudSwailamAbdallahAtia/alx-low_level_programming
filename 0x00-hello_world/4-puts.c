fcfb27a0eb10ed644fbc1ff2885a07bbbadcd555
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-10 09:38:38 -0800

    how to put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,5 +2,6 @@
 /*description for function main  here*/
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
-return (0); /*for the function*/
+return (0);
+/*for the function*/
 }
