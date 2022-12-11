3162cd4dc36ccbb20b6584193681260f442f8e5f
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-11 02:36:38 -0800

    write from a to z loewrcase with putchar only

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main- print alphapet in lowercase
+ *Return: 0
+ */
+int main (void)
+{
+for (char alpha = "a"; alpha <= "z"; alpha++)
+{
+putchar(alpha);
+}
+putchar("\n");
+return (0);
+}
