b620080fe35cde45397fbe57ff817dceba580d76
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-10 20:57:53 -0800

    if condition with c language

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <string.h>
 /**
  *main- condition if
  *Return: 0
  */
 int main(void)
 {
-  int n;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 if(n > 0)
 {printf("%dis positive\n", n);
 }
