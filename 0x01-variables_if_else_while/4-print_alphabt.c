085c4d1d8742ce7d99835640bf4289a5c3d968ad
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-11 04:19:57 -0800

    dafd

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,9 @@ int main(void)
 char alpha = 'a';
 while (alpha <= 'z')
 {
-if (alpha != 'q' && alpha != 'e')
-{
-putchar(alpha);
-alpha++;
-}
+  if (alpha != 'q' && alpha != 'e'){
+    putchar(alpha);
+    alpha++;}
 }
 putchar('\n');
 return (0);
