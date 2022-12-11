6972a9b6f521376179c58425727472ed04442851
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-11 05:58:11 -0800

    char from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main- for digits from 0 to 9
+ *Return: 0
+ */
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+char n;
+putchar(n);
+int n;
+n++;
+}
+putchar('\n');
+return (0);
+}
