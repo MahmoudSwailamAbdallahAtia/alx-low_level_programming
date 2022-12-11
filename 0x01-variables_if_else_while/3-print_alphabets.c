adebbde3757000313e0c1ca2c2b10573878acc7d
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-11 03:05:34 -0800

    lower and upper case with putchar with c language

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- *main- print alphapet in lowercase and then uppercase
+ *main- print alphapet in lowercase
  *Return: 0
  */
 int main(void)
 {
 char alpha = 'a';
-char Alpha = 'A';
-while (alpha <= 'z' && Alpha <= 'Z')
+while (alpha <= 'z')
 {
 putchar(alpha);
-putchar(Alpha);
 alpha++;
+}
+char Alpha = 'A';
+while (Alpha <= 'Z')
+{
+putchar(Alpha);
 Alpha++;
 }
 putchar('\n');
