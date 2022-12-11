f30c1947497a2b16d36c75396706dc81f35b9618
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-11 05:49:44 -0800

    digits from 0 to 9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,12 +4,14 @@
  *main- for digits from 0 to 9
  *Return (0)
  */
-int main (void){
-  int n = 0;
-  while (n < 10){
-    printf("%d", n);
-    n++;
-  }
-  putchar('\n');
-  return (0);
+int main (void)
+{
+int n = 0;
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+putchar('\n');
+return (0);
 }
