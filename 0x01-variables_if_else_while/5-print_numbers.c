cdd3e0a5b6d3e6c79a978f296d80d4ad13703640
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-11 05:34:21 -0800

    print digits some

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main- for digits from 0 to 9
+ *Return (0)
+ */
+int main (void){
+  for (int n = 0; n < 10; n++){
+    printf("%d\n", n);
+  }
+  return (0);
+}
