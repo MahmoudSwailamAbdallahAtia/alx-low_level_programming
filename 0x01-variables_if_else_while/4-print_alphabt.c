f77cb1f58e0bfcf75698e296bfb49dfa9a797ce7
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-11 03:42:11 -0800

    try

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,11 @@ int main(void)
 char alpha = 'a';
 while (alpha <= 'z')
 {
-if (alpha == 'q' || alpha == 'e')
-{ 
+if (alpha == 'q' || alpha == 'e') 
 continue;
-}
 else
-{
 putchar(alpha);
 alpha++;
-}
 }
 putchar('\n');
 return (0);
