b0394b0e2466fc5166902fb3e84e1155de346ca4
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-12 21:59:38 -0800

    start a new directory

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - print putchar
+ *Return: 0
+ */
+void funprint ()
+{
+  char word = '_putchar';
+  char i;
+  for (i in word)
+    {
+      putchar(i);
+    }
+  putchar('\n');
+}
+int main(void)
+{
+  funprint;
+  return (0);
+}
