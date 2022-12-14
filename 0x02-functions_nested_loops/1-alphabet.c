917d711bb625333201f4a9f62805bc2aa846cff4
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-13 22:01:43 -0800

    try

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,8 +6,7 @@ void print_alphabet(void);
 void print_alphabet(void)
 {
 char letter;
-for (letter ='a'; letter <= 'z'; letter++)
-{putchar(letter);
+for(letter ='a'; letter <= 'z'; letter++){putchar(letter);
 }
 putchar('\n');
 }
