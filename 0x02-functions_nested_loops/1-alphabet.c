aab2a289c29e13df2073bbfdc8d418dff3eb41d6
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-14 01:44:15 -0800

    funpointer

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,24 @@
 #include "main.h"
 /**
+ * main - main functions
+ * pointer - function pointer
  * print_alphabet - print putchar
+ * Return: Always 0
  */
 void print_alphabet(void);
+int main(void)
+{
+void(*pointer)(void);
+pointer = print_alphabet;
+pointer();
+return (0);
+}
 void print_alphabet(void)
 {
 char letter;
-for(letter ='a'; letter <= 'z'; letter++){putchar(letter);
+for(letter ='a'; letter <= 'z'; letter++)
+{
+putchar(letter);
 }
 putchar('\n');
 }
