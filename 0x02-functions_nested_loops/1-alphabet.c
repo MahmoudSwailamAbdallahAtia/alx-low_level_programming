3cff5e0f2a984e6087431ff01b58adcf527183d7
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-13 22:11:41 -0800

    try

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- *print_alphabet - print putchar
+ * main - check the code for Holberton School students.
+ * Return: Always 0.
+ * print_alphabet - print putchar
  */
 void print_alphabet(void);
 void print_alphabet(void)
@@ -10,10 +12,6 @@ for(letter ='a'; letter <= 'z'; letter++){putchar(letter);
 }
 putchar('\n');
 }
-/**
- * main - check the code for Holberton School students.
- * Return: Always 0.
- */
 int main(void)
 {
         print_alphabet();
