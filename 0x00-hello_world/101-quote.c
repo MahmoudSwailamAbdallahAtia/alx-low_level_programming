78929520ed41d0611c6fa7fd1073cc0a37ec24f1
MahmoudSwailamAbdallahAtia <mahmoudswailam183@gmail.com>
2022-12-22 09:10:06 -0800

    try

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
-/*
- * main - main function that prints txt
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  * Return: always 0
  */
 int main(void)
